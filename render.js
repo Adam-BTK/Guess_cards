afd82a03b1ea851426d9ee710a96aba1af05d60e
Adam Btk <mcxchr@yandex.ru>
2023-09-29 18:57:37 +0300

    init

--- render.js
+++ render.js
@@ -5,40 +5,43 @@ const level1AppElement = document.querySelector('.level1');
 const level2AppElement = document.querySelector('.level2');
 const level3AppElement = document.querySelector('.level3');
 const checkboxElement = document.getElementById('checkbox');
+// let selectedCard = null;
 
-// Определение функции для генерации случайных карт
-const generateRandomCards = (numberOfCards, deck) => {
-  const selectedCards = deck.slice(0, numberOfCards);
-  return selectedCards;
+const showCardFront = (cardElement) => {
+  cardElement.querySelector('.card-front').classList.add('visible');
+  cardElement.querySelector('.card-back').classList.remove('visible');
 };
 
-// Определение функции для отображения карт на игровом поле
+const hideCardFront = (cardElement) => {
+  cardElement.querySelector('.card-front').classList.remove('visible');
+  cardElement.querySelector('.card-back').classList.add('visible');
+};
+
 const displayCards = (cards, storedValue) => {
-  // Очистите игровой контейнер перед отображением новых карт
   level1AppElement.innerHTML = '';
   level2AppElement.innerHTML = '';
   level3AppElement.innerHTML = '';
 
+  const restartBtnElement = document.createElement('div');
+  restartBtnElement.classList.add('timer_btn_box');
+
   cards.forEach((card, index) => {
     const cardElement = document.createElement('div');
     cardElement.classList.add('card');
-    cardElement.dataset.index = index; // Храните индекс карты в атрибуте data
+    cardElement.dataset.index = index;
 
     const cardFront = document.createElement('div');
     cardFront.classList.add('card-front');
-    // Установите стиль для лицевой стороны карты (например, изображение масти и ранга)
 
     const cardBack = document.createElement('div');
     cardBack.classList.add('card-back');
-    // Установите стиль для обратной стороны карты (например, изображение "рубашки")
 
     cardElement.appendChild(cardFront);
     cardElement.appendChild(cardBack);
 
-    // Установите фоновое изображение на лицевой стороне карты в зависимости от её индекса
-    cardFront.style.backgroundImage = `url(./img/рубашка.png)`;
+    cardFront.style.backgroundImage = `url(./img/card${index + 1}.png)`;
+    cardBack.style.backgroundImage = `url(./img/рубашка.png)`;
 
-    // В зависимости от уровня сложности, добавьте карты на соответствующее игровое поле
     if (storedValue === 'level 1') {
       level1AppElement.appendChild(cardElement);
     } else if (storedValue === 'level 2') {
@@ -49,12 +52,10 @@ const displayCards = (cards, storedValue) => {
   });
 };
 
-// Определение функции для рендеринга игровой страницы
 export const renderGamePage = (storedValue) => {
   checkboxElement.style.display = 'none';
   playboxElement.style.display = 'block';
 
-  // Определите количество карт в зависимости от уровня сложности
   let numberOfCards;
   if (storedValue === 'level 1') {
     numberOfCards = 6;
@@ -66,15 +67,25 @@ export const renderGamePage = (storedValue) => {
 
   const deck = generateDeck();
   shuffleDeck(deck);
-  const selectedCards = generateRandomCards(numberOfCards, deck);
+  const selectedCards = deck.slice(0, numberOfCards);
 
-  displayCards(selectedCards);
+  displayCards(selectedCards, storedValue);
 
-  // Показывайте карты пользователю в течение 5 секунд
   setTimeout(() => {
-    // Скройте карты
-    // Добавьте обработчики кликов на карты и реализуйте логику сравнения
-  }, 5000);
+    level1AppElement.querySelectorAll('.card').forEach(hideCardFront);
+    level2AppElement.querySelectorAll('.card').forEach(hideCardFront);
+    level3AppElement.querySelectorAll('.card').forEach(hideCardFront);
 
-  // Добавьте обработчики кликов на карты и реализуйте логику сравнения
+    level1AppElement.querySelectorAll('.card').forEach((card) => {
+      card.addEventListener('click', () => showCardFront(card));
+    });
+
+    level2AppElement.querySelectorAll('.card').forEach((card) => {
+      card.addEventListener('click', () => showCardFront(card));
+    });
+
+    level3AppElement.querySelectorAll('.card').forEach((card) => {
+      card.addEventListener('click', () => showCardFront(card));
+    });
+  }, 5000);
 };
